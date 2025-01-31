a1a625559541a56e295ddc041d72f5b7627edab2
HNihad0 <153919479+HNihad0@users.noreply.github.com>
2025-01-31 13:11:57 +0400

    Splash screen created

--- lib/ui/pages/splash/splash_page.dart
+++ lib/ui/pages/splash/splash_page.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
-import '../../widgets/custom_app_bar.dart';
+import '../../../utils/constants/app_assets.dart';
+import '../../../utils/constants/app_paddings.dart';
+
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 3), () {
+      // Go.replace(context, Pager.onboard);
+    });
     return Scaffold(
-      appBar: const CustomAppBar(
-        title: 'Create an Account',
+      body: Center(
+        child: SvgPicture.asset(
+          AppAssets.logo,
+          width: 200,
+          height: 200,
+        ),
       ),
+      floatingActionButton: const Padding(
+        padding: AppPaddings.a32,
+        child: CircularProgressIndicator(),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
